2e6221a8ae3aa16f17bec032835b3cc301e47a32
Роман Кармацкий <Knispel.Kurt@gmail.com>
2022-03-25 02:53:39 +0300

    "Target is out of bounds" for criterion is fixed.

--- Source/Common/ModelLoaderModule.py
+++ Source/Common/ModelLoaderModule.py
@@ -1,16 +1,19 @@
 from torch import nn
 from torchvision import models
 import Common.Utils as utils
+from torchsummary import summary
 
 # Loader of model.
 class PretrainedModelLoader():
     def __init__(self):
-        utilites = utils.Utils()
+        utilites = utils.Utils('..\\..\\DataSet\\VGGDataSet\\FirstEpoche')
         target_device = utilites.load_device()
-        model = models.resnet50(pretrained = True, progress = False)
-        self.neural_network_model = nn.Sequential(*(list(model.children())[:-1]))
+        self.neural_network_model = models.resnet50(pretrained = False, progress = False)
+        last_layer = list(self.neural_network_model.children())[-1]
+        in_features_prelast_layer = last_layer.in_features
+        self.neural_network_model.fc = nn.Linear(in_features_prelast_layer, utilites.get_last_label() + 1)
         for param in self.neural_network_model.parameters():
-            param.requires_grad = False
+            param.requires_grad = True
 
         self.neural_network_model.to(target_device)
 
@@ -31,7 +34,11 @@ class PretrainedModelLoader():
         out_features_prelast_layer = utilites.get_last_label()
         in_features_function = nn.Linear(in_features_prelast_layer, out_features_prelast_layer)
         utilites = utils.Utils('..\\..\\DataSet\\VGGDataSet\\SecondEpoche')
-        out_features_function = nn.Linear(out_features_prelast_layer, utilites.get_last_label())
+        out_features_function = nn.Linear(out_features_prelast_layer, utilites.get_last_label() + 1)
         model.fc = nn.Sequential(in_features_function, nn.ReLU(), nn.Dropout(0.2), out_features_function, nn.LogSoftmax(dim = 1))
             
         return model
+
+    # Load nn info.
+    def summary(self):
+        print(summary(self.neural_network_model, (3, 256, 256)))
--- Source/Train/TrainService.py
+++ Source/Train/TrainService.py
@@ -1,16 +1,23 @@
 import torch
 from torch import optim, nn
 import Common.Utils as utilites
+import time
 
 # Service of model training.
 class TrainService():
+    # Initialization of train service.
     def __init__(self, model, learning_rate, only_last_layer: bool = False):
+        self.imager_per_iteration = 16
         self.utils = utilites.Utils()
         self.model = model
+        self.only_last_layer = only_last_layer
         if only_last_layer:
             self.optimizer = optim.Adam(model.classifier.parameters(), lr = learning_rate)
+            self.freeze_layers()
         else:
             self.optimizer = optim.Adam(model.parameters(), lr = learning_rate)
+            for param in self.model.parameters():
+                param.requires_grad = True
 
         self.criterion = nn.NLLLoss()
         self.device = self.utils.load_device()
@@ -36,16 +43,46 @@ class TrainService():
 
     # Call model forward for train dataset.
     def train(self, dataset_loader, steps):
+        print("Train process started...")
+        start = time.time()
         loss = 0.0
         self.model.train()
         for trdl_inputs, trdl_labels in dataset_loader.load():
             steps += 1
             inputs, labels = trdl_inputs.to(self.device), trdl_labels.to(self.device)
             self.optimizer.zero_grad()
-            logps = self.model.forward(inputs)
+            logps = self.convert_logs(self.model.forward(inputs))
             loss = self.criterion(logps, labels)
-            loss.backward()
+            if not self.only_last_layer:
+                loss.backward()
+
             self.optimizer.step()
             loss += loss.item()
+            print(f"Setep: {steps}; loss: {loss:.4f}; elapsed time: {time.time() - start:.2f} seconds.")
         
         return loss, steps
+    
+    # Преобразование тензора с убывающей ошибкой.
+    def convert_logs(self, logps):
+        result_tensor = torch.zeros(len(logps), len(logps[0]), dtype = logps.dtype)
+        for i in range(len(logps)):
+            for j in range(len(logps[0])):
+                result_tensor[i][j] = self.extract_data(logps[i][j], logps.dtype)
+        
+        return result_tensor
+
+    # Извлечение значения.
+    def extract_data(self, item, type):
+        if item.ndim > 1:
+            return self.extract_data(item[0], type)
+        return item
+
+    # Freeze layers till full connected layer.
+    def freeze_layers(self):
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        for param in self.model.fc.parameters():
+            param.requires_grad = True
+        
+        self.model
